182bcea195907b8a9c8d46be417ed987a6a403f1
Andrea <andrea.puccilog@gmail.com>
2025-04-24 14:28:32 +0200

    Updates

---
+++ precache-manifest.70f44f0ba44a0d94bcfec912e845765a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18cd5666e52e0ac13452e4e6dcd5b22e",
+    "url": "/Puccio7/MyCalendar/index.html"
+  },
+  {
+    "revision": "96a9e83f96003a4455b9",
+    "url": "/Puccio7/MyCalendar/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6593f894d016e85388cb",
+    "url": "/Puccio7/MyCalendar/static/js/2.c7821bdd.chunk.js"
+  },
+  {
+    "revision": "96a9e83f96003a4455b9",
+    "url": "/Puccio7/MyCalendar/static/js/main.90ce3e46.chunk.js"
+  },
+  {
+    "revision": "aa534f38b494dfbff475",
+    "url": "/Puccio7/MyCalendar/static/js/runtime~main.e55c5b38.js"
+  }
+]);
